63fe9e860e43bece9d562726522db7563ff6831a
Taek Been Nam <taekbeen93@gmail.com>
2021-05-07 23:17:20 -0400

    Set up form

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 console.log("Let's get this party started!");
 
-async function getSearchGif() {
-  const res = await axios.get('http://api.giphy.com/v1/gifs/search?q=hello&api_key=SOAltSiqBikDKqQ4uMljSyMdzIjm30wb&limit=5')
-  console.log(res.data.data[0])
-}
+const form = document.querySelector('.form');
+const input = document.querySelector('.form__input');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  getSearchGif(input.value);
+})
+
+async function getSearchGif(searchTerm) {
+  const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=SOAltSiqBikDKqQ4uMljSyMdzIjm30wb&limit=5`)
+  console.log(res.data.data[1])
+  const img = document.querySelector('#giphy');
+  img.src = res.data.data[0].images.downsized_medium.url
+};
+
+
