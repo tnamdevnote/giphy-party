346130164d8e43c92f60ce1a3ee9f218d037169e
Taek Been Nam <taekbeen93@gmail.com>
2021-05-09 14:30:39 -0400

    add delete function

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-console.log("Let's get this party started!");
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  getSearchGif(input.value);
-})
+const searchBtn = document.querySelector('#search');
+const deleteBtn = document.querySelector('#remove');
+
 
 async function getSearchGif(searchTerm) {
-  const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=SOAltSiqBikDKqQ4uMljSyMdzIjm30wb&limit=5`)
-  console.log(res.data.data[1])
-  const img = document.querySelector('#giphy');
-  img.src = res.data.data[0].images.downsized_medium.url
+  const res = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=SOAltSiqBikDKqQ4uMljSyMdzIjm30wb&limit=5`);
+  const imgURL = res.data.data[0].images.downsized_medium.url;
+  appendGif(imgURL);
 };
+
+const appendGif = (imgURL) => {
+  const gifCollection = document.querySelector('.collection');
+  const gif = document.createElement('img');
+  gif.setAttribute('class', 'collection__gif');
+  gif.src = imgURL;
+  gifCollection.append(gif);
+  
+  deleteBtn.addEventListener('click', () => {
+    gifCollection.removeChild(gif);
+  })
+}
+
+searchBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  getSearchGif(input.value.trim());
+})
+
 
 
